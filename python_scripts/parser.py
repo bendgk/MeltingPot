162cebc2580fd5b44a6556492f4a44a557ac7953
bendgk <benkosten@gmail.com>
2018-10-09 17:03:00 -0500

    Add python parser and load in vue

---
+++ python_scripts/parser.py
@@ -0,0 +1,61 @@
+import re
+import pprint
+from openpyxl import load_workbook
+import json
+
+pp = pprint.PrettyPrinter(indent=4)
+
+wb = load_workbook('data.xlsx')
+ws = wb.active
+
+ingredients = {}
+recipes = {}
+
+for col in [1, 2, 3, 4, 6, 7, 9]:
+    category = ""
+    
+    for row in range(2, 15):
+        cell_data = str(ws.cell(column=col, row=row).value).strip().lower()
+        if row == 2: category = cell_data
+        
+        elif cell_data != "none":
+            match = re.match(r"[a-zA-Z\/]\D*", cell_data)
+            
+            if match:
+                if category in ingredients:
+                    ingredients[category].append([match.group(0)[0:-1], 0])
+                else:
+                    ingredients[category] = [[match.group(0)[0:-1], 0]]
+                    
+for col in range(2, 27):
+    culture = ""
+    recipe = ""
+    
+    for row in range(17, 40):
+        cell_data = str(ws.cell(column=col, row=row).value).strip().lower()
+        
+        if row == 17:
+            culture = cell_data.strip()
+        elif row == 18:
+            recipe = cell_data.strip()
+            recipes[recipe] = {'culture': culture, 'ingredients': []}
+            
+        elif cell_data != "none":
+            if "(o)" in cell_data:
+                #order is recipes[rercipe] = [[Ingredient, Required], [Ingredient, Required]]
+                #where Ingredient is an ingredient and Required is a boolean
+                recipes[recipe]['ingredients'].append([cell_data.replace("(o)", "").strip(), False])
+            else:
+                recipes[recipe]['ingredients'].append([cell_data.strip(), True])
+                
+            for key, value in ingredients.items():
+                for food in value:
+                    if food[0] in cell_data:
+                        food[1] += 1
+
+with open('ingredients.json', 'w+') as f:
+    json.dump(ingredients, f, indent=4)
+
+
+with open('recipes.json', 'w+') as f:
+    json.dump(recipes, f, indent=4)
--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,66 @@
-window.addEventListener("load", function(event) {
-    var app = new Vue({
-        el: '#cookbook',
-        data: {
-            // included ingredients, starts off as list of total ingredients
-            unincludedIngredients:["Lettuce",
-            "Fish",
-            "Noodles"
-            ],
-            // array of currently included ingredients by name
-            includedIngredients:[],
-            // array of recipes, [0] is recipe name/identifier, [1] is boolean representing whether or not it is currently being displayed, [2] is array of ingredients by name
-            recipes:[
-                ["Fish Noodles", true, ["Fish", "Noodles"]],
-                ["Lettuce Fish", true, ["Lettuce", "Fish"]],
-                ["Lettuce Noodles", true, ["Lettuce", "Noodles"]],
-                ["Lettuce Fish Noodles", true, ["Lettuce", "Fish", "Noodles"]],
-            ],
-        },
-        methods: {
-            /**
-             * Prunes any recipes that exclude selected ingredients (using global variables so no parameters)
-             */
-            pruneUnselectedIngredients: function () {
-                for (var recipe of this.recipes){
-                    var display = true;
-                    for (var ingredient of this.includedIngredients){
-                        if(!recipe[2].includes(ingredient)){
-                            display = false;
-                            break;
-                        }
+var app = new Vue({
+    el: '#cookbook',
+    data: {
+        // included ingredients, starts off as list of total ingredients
+        ingredients: {},
+        
+        unincludedIngredients:["Lettuce",
+        "Fish",
+        "Noodles"
+        ],
+        // array of currently included ingredients by name
+        includedIngredients:[],
+        // array of recipes, [0] is recipe name/identifier, [1] is boolean representing whether or not it is currently being displayed, [2] is array of ingredients by name
+        recipes:[
+            ["Fish Noodles", true, ["Fish", "Noodles"]],
+            ["Lettuce Fish", true, ["Lettuce", "Fish"]],
+            ["Lettuce Noodles", true, ["Lettuce", "Noodles"]],
+            ["Lettuce Fish Noodles", true, ["Lettuce", "Fish", "Noodles"]],
+        ],
+    },
+    methods: {
+        /**
+         * Prunes any recipes that exclude selected ingredients (using global variables so no parameters)
+         */
+        pruneUnselectedIngredients: function () {
+            for (var recipe of this.recipes){
+                var display = true;
+                for (var ingredient of this.includedIngredients){
+                    if(!recipe[2].includes(ingredient)){
+                        display = false;
+                        break;
                     }
-                    recipe[1] = display;
                 }
-            },
-            /**
-             * Toggles ingredient between using/not using and checks what recipes to display
-             * @param string ingredient representing ingredient to toggle
-             */
-            toggleIngredient: function(ingredient) {
-                if(this.includedIngredients.includes(ingredient)){
-                    this.unincludedIngredients.push(ingredient);
-                    this.includedIngredients.splice(this.includedIngredients.indexOf(ingredient), 1);
-                }
-                else{
-                    this.includedIngredients.push(ingredient);
-                    this.unincludedIngredients.splice(this.unincludedIngredients.indexOf(ingredient), 1);
-                }
-                this.pruneUnselectedIngredients();
-            },
-            /**
-             * Gets image path using name
-             * @param string name representing name of image to find
-             * @return string path full image path
-             */
-            getImagePath: function(name){
-                return "Images/" + name + ".jpg";
-            },
-        }
-    })
+                recipe[1] = display;
+            }
+        },
+        /**
+         * Toggles ingredient between using/not using and checks what recipes to display
+         * @param string ingredient representing ingredient to toggle
+         */
+        toggleIngredient: function(ingredient) {
+            if(this.includedIngredients.includes(ingredient)){
+                this.unincludedIngredients.push(ingredient);
+                this.includedIngredients.splice(this.includedIngredients.indexOf(ingredient), 1);
+            }
+            else{
+                this.includedIngredients.push(ingredient);
+                this.unincludedIngredients.splice(this.unincludedIngredients.indexOf(ingredient), 1);
+            }
+            this.pruneUnselectedIngredients();
+        },
+        /**
+         * Gets image path using name
+         * @param string name representing name of image to find
+         * @return string path full image path
+         */
+        getImagePath: function(name){
+            return "Images/" + name + ".jpg";
+        },
+    },
+    created() {
+        $.getJSON('python_scripts/ingredients.json').then((data) => {
+            this.ingredients = data;
+        });
+    }
 });
