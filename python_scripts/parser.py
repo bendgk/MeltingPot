4fdef389fdc8afcc0b13e8a1af89251ae618cd43
bendgk <benkosten@gmail.com>
2018-10-14 20:14:00 -0500

    added tmp images for ingredients

--- python_scripts/parser.py
+++ python_scripts/parser.py
@@ -2,6 +2,7 @@ import re
 import pprint
 from openpyxl import load_workbook
 import json
+from PIL import Image, ImageDraw
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -59,3 +60,15 @@ with open('ingredients.json', 'w+') as f:
 
 with open('recipes.json', 'w+') as f:
     json.dump(recipes, f, indent=4)
+    
+for value in ingredients.values():
+    for ingredient in value:
+        ingredient = ingredient[0].replace(" ", "_")
+        ingredient = ingredient.replace("/", "-")
+        
+        print(ingredient)
+        
+        img = Image.new('RGB', (200, 200), color = (0, 0, 0))
+        d = ImageDraw.Draw(img)
+        d.text((100,100), ingredient, fill=(255,255,255))
+        img.save('images/' + ingredient + ".jpg")
